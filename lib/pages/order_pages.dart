8b88eee32035dc35c3c0b452284d60936aaff184
andresfaj <achuryjaramillo@gmail.com>
2019-12-11 01:55:17 -0500

    Pedido en curso ready

--- lib/pages/order_pages.dart
+++ lib/pages/order_pages.dart
@@ -9,6 +9,7 @@ import 'package:timugo_client_app/providers/register_provider.dart';
 import 'package:timugo_client_app/providers/sqlite_providers.dart';
 
 import 'model_order.dart';
+import 'recent_transaction_pages.dart';
 
 class Order extends StatefulWidget {
   Order({Key key, this.title}) : super(key: key);
@@ -43,12 +44,10 @@ class _MyHomePageState extends State<Order> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold(
-      
+    return Scaffold(      
       bottomNavigationBar: Container(
         height: 200,
         decoration: BoxDecoration(
@@ -66,25 +65,24 @@ class _MyHomePageState extends State<Order> {
                   Container(
                     height: 0,
                     width: 0,
-                  child:FutureBuilder<List<DataClient>>(
-                  future: ClientDB.db.getClient(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  Text('');
-                    if(snapshot.hasData) {
-                     DataClient item = snapshot.data[0];
-                     order.idClient=item.id;
-                     order.address=item.address;
-                     order.typeService=1;
-                     print(order);
-                    }
-                    Text('');
-                    }
-                  
+                    child:FutureBuilder<List<DataClient>>(
+                      future: ClientDB.db.getClient(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        Text('');
+                        if(snapshot.hasData) {
+                        DataClient item = snapshot.data[0];
+                        order.idClient=item.id;
+                        order.address=item.address;
+                        order.typeService=1;
+                        print(order);
+                        }
+                        Text('');
+                      }
                     ),
                   ),
-                    Text('Hay actualmente'+' '+rnd.toString()+' '+'barberos en tu sector ...',style: TextStyle( color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w800),),
-                     SizedBox(height: 20.0),
-                    GoButton(
+                  Text('Hay actualmente'+' '+rnd.toString()+' '+'barberos en tu sector ...',style: TextStyle( color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w800),),
+                  SizedBox(height: 20.0),
+                  GoButton(
                       title: "Pedir",
                       onPressed:() 
                        {if (state = false){
@@ -108,15 +106,11 @@ class _MyHomePageState extends State<Order> {
                                     res.then((response) async {
                                       print(res);
                                       if (response['response'] == 2){
-                                
-                                       Navigator.pushNamed(context, 'transaction');
+                                        Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => RecentTransactionsPage()));                                
+              
                                       //    builder: (context) => Login()));
-
-
                                       }
-
                                     });
-                                  
                                 },
                               ),
                             ],
@@ -397,8 +391,4 @@ class _GoButtonState extends State<GoButton> {
       ],
     );
   }
-
-
-  
 }
-
