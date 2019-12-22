ddc546427f9e88c5fba87d3faeb872a02d4bb490
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2019-12-22 00:06:42 -0500

    Main integration with push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,33 @@ import 'package:provider/provider.dart';
 import 'package:timugo/src/pages/codeVerification_page.dart';
 import 'package:timugo/src/pages/login_pages.dart';
 import 'package:timugo/src/pages/registerData_page.dart';
+import 'package:timugo/src/providers/push_notifications_provider.dart';
 import 'package:timugo/src/providers/user.dart';
 
 
  
 void main() => runApp(MyApp());
  
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+
+    final pushProvider = PushNotificationProvider(); 
+    pushProvider.initNotifications();
+
+    pushProvider.messages.listen((argument){
+      print("Argumento");
+      print(argument);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -25,7 +45,6 @@ class MyApp extends StatelessWidget {
           'login':(context)=> LoginPage(),
           'code':(context)=> Code(),
           'registerData':(context)=> RegisterData()
-
         
         },
       )
