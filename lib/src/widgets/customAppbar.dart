cc7d7b7394d7fce6f1f5a67ffc422eb423d1ac9d
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2019-12-28 16:34:04 -0500

    custom app bar modifications

--- lib/src/widgets/customAppbar.dart
+++ lib/src/widgets/customAppbar.dart
@@ -10,38 +10,27 @@ class CustomAppBar extends StatelessWidget {
       color: Colors.transparent,
       child: Row(
         children: <Widget>[
-          IconButton(icon:Icon(FontAwesomeIcons.userCircle),onPressed: (){},),
-          Spacer(),
-          IconButton(icon:Icon(FontAwesomeIcons.search),onPressed: (){},),
-          Stack(
-            children: <Widget>[
-             IconButton(icon:Icon(FontAwesomeIcons.headset),onPressed: (){},),
-             Container(
-               child: Center(child: Text('1',style: TextStyle(color: Colors.white ),)),
-               width: 20.0,
-               height: 20.0,
-               decoration: BoxDecoration(
-                 color: Colors.red,
-                 borderRadius: BorderRadius.circular(20.0)
-               ),
-             )
+                        SizedBox(width: 15.0,),
+                        IconButton(icon:Icon(FontAwesomeIcons.userCircle,size: 25.0,color: Colors.black),onPressed: (){},), 
+                        Spacer(),
+                        Stack(
+                          children: <Widget>[
+                          IconButton(icon:Icon(FontAwesomeIcons.headset,color: Colors.black,),onPressed: (){},),
+                          Container(
+                            child: Center(child: Text('1',style: TextStyle(color: Colors.white ),)),
+                            width: 20.0,
+                            height: 20.0,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20.0)
+                            ),
+                          )
 
-            ],
-          ),
-
-
-
-          IconButton(icon:Icon(FontAwesomeIcons.ellipsisV,size: 15.0,),onPressed: (){},),
-
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Container(
-              width: 50.0,
-              height: 50.0,
-              color: Colors.blue,
-            ),
-          ),
-          SizedBox(width: 15.0,)
+                          ],
+                        ),
+                        Icon(FontAwesomeIcons.ellipsisV,size: 15.0,color: Colors.black), 
+                        IconButton(icon:Icon(FontAwesomeIcons.ticketAlt,size: 15.0,color: Colors.black),onPressed: (){},), 
+                        SizedBox(width: 8.0,)
 
 
         ],
