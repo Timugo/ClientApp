3e0ce40ebcac6553e62390db4927d7ffa43e9fc1
Jeisson Santacruz <jeissonsantacruz@javerianacali.edu.co>
2020-04-25 14:37:32 -0500

    add   skinit for payment

--- lib/src/pages/Payment.dart
+++ lib/src/pages/Payment.dart
@@ -1,26 +1,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:timugo/src/models/card_model.dart';
 import 'package:timugo/src/models/nequi_model.dart';
 import 'package:timugo/src/pages/nequi_page.dart';
 import 'package:timugo/src/pages/pse_page.dart';
 import 'package:timugo/src/preferencesUser/preferencesUser.dart';
+import 'package:timugo/src/providers/user.dart';
 import 'package:timugo/src/services/number_provider.dart';
 import 'package:timugo/src/widgets/creditCard.dart';
 
 class Payment extends StatelessWidget {
   const Payment({Key key}) : super(key: key);
 
-  void addPrincipal(String value){
-    final prefs = new PreferenciasUsuario();  //this function add the principal address of user
-    print('estpy selecionando');
+  void addPrincipal(String value,BuildContext context){
+    final  userInfo = Provider.of<UserInfo>(context);
+    final prefs = new PreferenciasUsuario();
+      //this function add the principal address of user
+    userInfo.payment =value;
     prefs.payment= value;
+
    // addFavorite.seendFavorite(value);
   }
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
@@ -83,6 +88,10 @@ class Payment extends StatelessWidget {
         leading: icon ? Icon(FontAwesomeIcons.solidCreditCard,color: Colors.black,):Image( image:AssetImage(image),width: 55,),
         trailing: Icon(FontAwesomeIcons.arrowRight,color: Colors.black,) ,
         onTap: (){
+          if (icon == true) {
+            texto = 'Tarjeta';
+          }
+          addPrincipal(texto,context);
           Navigator.push(
             context,MaterialPageRoute(
             builder: (context) => ruta
@@ -108,7 +117,7 @@ class Payment extends StatelessWidget {
             
             itemCount: productos.length,
             itemBuilder: (context, i) => ListTileItemNequi(
-            addPrincipal: ()=> addPrincipal('Nequi'),
+            addPrincipal: ()=> addPrincipal('Nequi',context),
               producto: productos[i],)
               
           );
@@ -122,84 +131,80 @@ class Payment extends StatelessWidget {
    Widget _createCard(){
     final aditionalProvider = GetUserPayments();
     return FutureBuilder(
-        future: aditionalProvider.getPayments(),
-        builder: (BuildContext context, AsyncSnapshot<List<CardModel>> snapshot) {
-          if ( snapshot.hasData ) {
-            final productos = snapshot.data;
-            return  ListView.builder(
-                padding: EdgeInsets.only(left: 15,right: 15),
-                shrinkWrap: true,
-              key: UniqueKey(),
-              itemCount: productos.length,
-              itemBuilder: (context, i) => ListTileItem(
-              addPrincipal: ()=> addPrincipal('Card')
-              , producto: productos[i],)
-                
-            );
-          }else {
-            return Center( child: CircularProgressIndicator());
-          }
-        },
-      );
+      future : aditionalProvider.getPayments(),
+      builder: (BuildContext context, AsyncSnapshot<List<CardModel>> snapshot) {
+        if ( snapshot.hasData ) {
+          final productos = snapshot.data;
+          return ListView.builder(
+            padding    : EdgeInsets.only(left: 15,right: 15),
+            shrinkWrap : true,
+            key        : UniqueKey(),
+            itemCount  : productos.length,
+            itemBuilder: (context, i) => ListTileItem(
+              addPrincipal: ()=> addPrincipal('Tarjeta',context),
+              producto    : productos[i],
+            )
+          );
+        }else {
+          return Center( child: CircularProgressIndicator());
+        }
+      },
+    );
   }
-
-
-
 }
 class ListTileItem extends StatelessWidget {
+
   final CardModel producto;
   final Function addPrincipal;
+
   ListTileItem({this.producto,this.addPrincipal});
 
   @override
   Widget build(BuildContext context) {
-   Color color = producto.favorite ? Colors.white : Colors.black;
-    print(producto.favorite);
+    Color color = producto.favorite ? Colors.white : Colors.black;
     return  Card(
-      
       shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
       ),
       elevation: 5,
       child: Ink(
-                decoration: BoxDecoration(
-                    gradient:  producto.favorite == false?  LinearGradient(colors: [Colors.white,Colors.white]) :  LinearGradient(colors: [Color(0xFF19AEFF), Color(0xFF139DF7),Color(0xFF0A83EE),Color(0xFF0570E5),Color(0xFF0064E0)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0)
-                ),
-      child: ListTile(
-                contentPadding: EdgeInsets.only(left:30,right: 15),
-
-        
-        title:Text(producto.fullName,style: TextStyle(color:color),),
-        subtitle: Text('**************'+producto.last4Numbers, style: TextStyle(color: color,fontSize: 15,fontWeight: FontWeight.bold)),
-        leading:_chooseCard(producto.brand,color) ,
-        trailing:  producto.favorite ? Icon(FontAwesomeIcons.check,color: color,):  null,
-        onTap: addPrincipal
-      )
+        decoration: BoxDecoration(
+          gradient: producto.favorite == false?  LinearGradient(colors: [Colors.white,Colors.white]) :  LinearGradient(colors: [Color(0xFF19AEFF), Color(0xFF139DF7),Color(0xFF0A83EE),Color(0xFF0570E5),Color(0xFF0064E0)],
+            begin : Alignment.centerLeft,
+            end   : Alignment.centerRight,
+          ),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.only(left:30,right: 15),
+          title   : Text(producto.fullName,style: TextStyle(color:color),),
+          subtitle: Text('**************'+producto.last4Numbers, style: TextStyle(color: color,fontSize: 15,fontWeight: FontWeight.bold)),
+          leading : _chooseCard(producto.brand,color) ,
+          trailing: producto.favorite ? Icon(FontAwesomeIcons.check,color: color,):  null,
+          onTap   : addPrincipal
+        )
       )
     );
-  
   }
+
   Icon _chooseCard( card,color){
     if(card ==  'VISA'){
-         return  Icon( FontAwesomeIcons.ccVisa,color: color,);
+      return  Icon( FontAwesomeIcons.ccVisa,color: color,);
     }
     if (card ==  'MASTERD_CARD'){
       return  Icon( FontAwesomeIcons.ccMastercard,color: color);
     }
-     else{
-       return Icon( FontAwesomeIcons.ccAmex,color: color);
-     }
-
+    else{
+      return Icon( FontAwesomeIcons.ccAmex,color: color);
+    }
   }
 
 }
 class ListTileItemNequi extends StatelessWidget {
+
   final NequiModel producto;
   final Function addPrincipal;
+
   ListTileItemNequi({this.producto,this.addPrincipal});
   @override
   Widget build(BuildContext context) {
@@ -209,28 +214,22 @@ class ListTileItemNequi extends StatelessWidget {
         borderRadius: BorderRadius.circular(20.0),
       ),
       elevation: 5,
-       child: Ink(
-                decoration: BoxDecoration(
-                    gradient:  producto.favorite == false?  LinearGradient(colors: [Colors.white,Colors.white]) :  LinearGradient(colors: [Color(0xFF19AEFF), Color(0xFF139DF7),Color(0xFF0A83EE),Color(0xFF0570E5),Color(0xFF0064E0)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0)
-                ),
-                child: ListTile(
-        title: Text(producto.phone,style:  TextStyle(color:color,fontWeight: FontWeight.bold )),
-        leading:Image( image:AssetImage('assets/images/Nequi.png'),width: 55,),
-        subtitle: Text(producto.type, style: TextStyle(color: color, )),
-        trailing:  producto.favorite ? Icon(FontAwesomeIcons.check,color: color,):  null,
-          onTap: addPrincipal,
-      
+      child: Ink(
+        decoration: BoxDecoration(
+          gradient:  producto.favorite == false?  LinearGradient(colors: [Colors.white,Colors.white]) :  LinearGradient(colors: [Color(0xFF19AEFF), Color(0xFF139DF7),Color(0xFF0A83EE),Color(0xFF0570E5),Color(0xFF0064E0)],
+          begin   : Alignment.centerLeft,
+          end     : Alignment.centerRight,
+          ),
+          borderRadius: BorderRadius.circular(20.0)
         ),
-              ),
-      
-         
-       
-     
-        );
-  
+        child: ListTile(
+          title   : Text(producto.phone,style:  TextStyle(color:color,fontWeight: FontWeight.bold )),
+          leading :Image( image:AssetImage('assets/images/Nequi.png'),width: 55,),
+          subtitle: Text(producto.type, style: TextStyle(color: color, )),
+          trailing:  producto.favorite ? Icon(FontAwesomeIcons.check,color: color,):  null,
+          onTap: addPrincipal,
+        ),
+      ),
+    );
   }
 }
