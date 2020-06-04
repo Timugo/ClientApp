8ce19b77674a3d4076806706277abb3daf1d1ebc
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-06-04 12:51:01 -0500

    chore(): fix globals name

---
+++ lib/globals.dart
@@ -0,0 +1,5 @@
+library my_prj.globals;
+/* Production URL */
+//String url = 'https://api.timugo.com/';
+/* Sanbox URL */
+String url ='https://sandbox.timugo.com/'; 
--- lib/globlas.dart
+++
@@ -1,5 +0,0 @@
-library my_prj.globals;
-/* Production URL */
-String url = 'https://api.timugo.com/';
-/* Sanbox URL */
-//String url ='https://sandbox.timugo.com/'; 
