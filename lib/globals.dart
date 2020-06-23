35a91ba363369d2ca9e75b0e815bca2361e2034b
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-06-23 00:12:06 -0500

    chore(): improve documentation

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,8 @@
+/*
+  This file contains al the enviroment variables to switch
+  between Production and DEVELOPMENT mode
+*/
+// FLutter dependencies
 library my_prj.globals;
 /* Production URL */
 //String url = 'https://api.timugo.com/';
