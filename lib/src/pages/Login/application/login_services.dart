5bbbb9619d11b46494a2f5707ac4492076160eb9
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-06-28 10:40:25 -0500

    fix(Login): correction of hexagonal architecture

--- lib/src/pages/Login/application/login_services.dart
+++ lib/src/pages/Login/application/login_services.dart
@@ -1,12 +1,15 @@
+//flutter dependencies
 import 'package:http/http.dart' as http;
-import 'package:timugo/globlas.dart' as globals;
+import 'package:timugo/globals.dart' as globals;
 import 'dart:convert';
-
+// Preferences
 import 'package:timugo/src/preferencesUser/preferencesUser.dart';
 
 final String urlBase = globals.url;
 
-/* Class that create a user  */
+/* 
+  Class that create a user 
+*/
 class RegisterUser {
   /* Request Url  */
   final String url = urlBase + 'loginUserV2';
@@ -24,24 +27,34 @@ class RegisterUser {
     return jsonDecode(response.body);
   }
 }
-/* Class that get the user information */
+
+/*
+  Class that get the user information
+*/
 class GetUserInformation {
+  // Properties
   final String url = urlBase + 'getUser';
   final prefs = PreferenciasUsuario();
 
   Future<Map<String, dynamic>> getName(String phone) async {
     var _urlcode = url + '?phone=' + phone;
-    // make POST request
-    http.Response response = await http.get(_urlcode);
-    final decodeData = jsonDecode(response.body);
-    // put the  user information into  internal storage
-    if (decodeData['response'] == 2) {
-      prefs.name = decodeData['content']['name'].toString();
-      prefs.pts = decodeData['content']['points'].toString();
-      prefs.id = decodeData['content']['id'].toString();
-      prefs.email = decodeData['content']['email'].toString();
-      //  userInfo.pts=decodeData['content']['points'];
-    }
-    return decodeData;
+    // http.Response response = await http.get(_urlcode);
+    //final decodeData = jsonDecode(response.body);
+
+    //POST request (Need to be improved, servives only returns a https requests)
+    http.get(_urlcode).then((response) {
+      final decodeData = jsonDecode(response.body);
+      if (decodeData['response'] == 2) {
+        prefs.name = decodeData['content']['name'].toString();
+        prefs.pts = decodeData['content']['points'].toString();
+        prefs.id = decodeData['content']['id'].toString();
+        prefs.email = decodeData['content']['email'].toString();
+      }
+
+      return decodeData;
+    }).catchError((err) {
+      //error handle
+      print(err);
+    });
   }
 }
---
+++ lib/src/pages/Login/domain/user_model.dart
@@ -0,0 +1,28 @@
+// Flutter dependencies
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+UserModel registerUserModelFromJson(String str) =>
+    UserModel.fromJson(json.decode(str));
+
+String registerUserModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel with ChangeNotifier {
+  int phone;
+  String name;
+  String email;
+  UserModel({
+    this.phone,
+    this.name = '',
+    this.email = '',
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+      phone: (json["phone"]), email: json["email"], name: json["pass"]);
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "phone": phone.toString(),
+        "email": email,
+      };
+}
