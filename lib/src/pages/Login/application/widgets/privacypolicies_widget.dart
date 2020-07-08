9ac852ab46112080ac74e20e0027407f86f8e1a1
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-07 21:55:17 -0500

    fix(Politics):  fix outdated privacy politic link

--- lib/src/pages/Login/application/widgets/privacypolicies_widget.dart
+++ lib/src/pages/Login/application/widgets/privacypolicies_widget.dart
@@ -1,21 +1,21 @@
+//Flutter and Dart dependencies
 import 'dart:async';
 import 'package:flutter/material.dart';
-
+// Plugins
 import 'package:webview_flutter/webview_flutter.dart';
 
 class PrivacyPoliticies extends StatelessWidget {
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
-
+  final Completer<WebViewController> _controller =Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: WebView(
-      initialUrl: 'https://timugo.tk/politics/privacy.html',
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webViewController) {
-        _controller.complete(webViewController);
-      },
-    ));
+      body: WebView(
+        initialUrl: 'https://api.timugo.com/politics/privacy.html',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      )
+    );
   }
 }
