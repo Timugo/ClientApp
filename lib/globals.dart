2691e0a9f4e9201dcd57245dcdeeea17f244a520
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-08 15:39:51 -0500

    chore(): add new url Apis V2

--- lib/globals.dart
+++ lib/globals.dart
@@ -5,9 +5,11 @@
 // FLutter dependencies
 library my_prj.globals;
 /* Production URL */
-// String url = 'https://api.timugo.com/';
+String url = 'https://api.timugo.com/';
+String urlV2 = 'https://apiv2.timugo.com/';
 /* Sanbox URL */
-String url ='https://sandbox.timugo.com/'; 
+//String url ='https://sandbox.timugo.com/'; 
+//String urlV2 ='https://sandboxv2.timugo.com/'; 
 
 /*data url*/
 String dataUrl = 'https://data.timugo.com/';
