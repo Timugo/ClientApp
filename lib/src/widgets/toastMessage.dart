d3b75f0b3194d0eacc3e89b411c6637453221d57
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-18 20:37:24 -0500

    chore(): improve toast documentation

--- lib/src/widgets/toastMessage.dart
+++ lib/src/widgets/toastMessage.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-void showToast(String message, Color backgcolor,) {
+
+void showToast(String message, Color color,) {
   // show the toast message in bell appbar
   Fluttertoast.showToast(
-      msg: message,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 1,
-      backgroundColor: backgcolor,
-      textColor: Colors.white,
-      fontSize: 14.0);
+    msg: message,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: color,
+    textColor: Colors.white,
+    fontSize: 14.0
+  );
 }
