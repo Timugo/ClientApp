4ee055f47b6642191294d7f262cd09f731f255f4
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-21 12:01:20 -0500

    chore(): improve de login Service

--- lib/src/pages/Login/infrastructure/login_services.dart
+++ lib/src/pages/Login/infrastructure/login_services.dart
@@ -1,5 +1,4 @@
 //flutter dependencies
-import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:http/http.dart' as http;
@@ -7,7 +6,6 @@ import 'package:timugo/globals.dart' as globals;
 import 'dart:convert';
 // Preferences
 import 'package:timugo/src/preferencesUser/preferencesUser.dart';
-import 'package:timugo/src/widgets/toastMessage.dart';
 
 final String urlBase = globals.urlV2;
 
@@ -15,30 +13,23 @@ final String urlBase = globals.urlV2;
   Class that contains the  login services
 */
 class LoginServices {
-  /* Request Url  */
-  final String url = globals.urlV2 + 'user/signup?';
+  
   final prefs = PreferenciasUsuario();
-
-  Future<Map<String, dynamic>> singUp(int phone, String name ,String email,String method, String publicityMethods ) async {
+  Future<http.Response> singUp(int phone, String name ,String email,String method, String publicityMethods ) async {
+    // URL
+    final String url = urlBase + 'user/signup?method=$method';
     /* Headers */
-    final String urlFinal = url+'method=$method';
     Map<String, String> headers = {"Content-Type": "application/json"};
     /* Body */
     var data = { "name": name,"phone": phone,"email":email,"publicityMethod":publicityMethods};
-    /* Response */
-    final encodedData = json.encode(data);
+    final body = json.encode(data);
     // making  POST request
-    print(urlFinal);
-    http.Response response =
-        await http.post(urlFinal, headers: headers, body: encodedData);
-    if (response.statusCode != 2){
-      showToast("Sucedío un error!", Colors.red);
-    }
-     return jsonDecode(response.body);
+    return await http.post(url, headers: headers, body: body);
   }
 
-  final String urlGetUser = urlBase + 'getUser';
+  
   Future<Map<String, dynamic>> getName(String phone) async {
+    final String urlGetUser = urlBase + 'getUser';
     var _urlcode = urlGetUser + '?phone=' + phone;
     http.Response response = await http.get(_urlcode);
 
