92a2b8d4c27fd71059c574276fdbdaac12130d63
Jeisson Santacruz <jeissonsantacruz@javerianacali.edu.co>
2020-07-20 23:38:05 -0500

    Register data user and sing in 100%

--- lib/src/pages/register/application/registerData_page.dart
+++ lib/src/pages/register/application/registerData_page.dart
@@ -1,5 +1,8 @@
 //flutter dependencies
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 /* Models*/
@@ -34,13 +37,27 @@ class _LoginPageState extends State<RegisterUserData> {
 
   @override
   Widget build(BuildContext context) {
+    final userInfo = Provider.of<UserInfo>(context);
     final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Padding(
           padding: EdgeInsets.all(size.height * 0.05),
           child: SingleChildScrollView(
               child: Column(children: <Widget>[
-                  formUserData(context)
+                userInfo.registerMethod == "PHONE" ?
+                  formUserData(context) : _numberForm(context),
+                   MyCustomButtoms(
+                hintText: "              Entrar",
+                icon: FontAwesomeIcons.signInAlt,
+                colors: [
+                  Color(0xFF19AEFF),
+                  Color(0xFF139DF7),
+                  Color(0xFF0A83EE),
+                  Color(0xFF0570E5),
+                  Color(0xFF0064E0)
+                ],
+                onPressed: (){sendCommet(context,userInfo.name,userInfo.email,userInfo.registerMethod,userInfo.publi);},
+              )
                 ]
               )
           )
@@ -55,8 +72,8 @@ class _LoginPageState extends State<RegisterUserData> {
     return Form(
         key: _formKey,
         child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            
+            
             children: <Widget>[
               Text(
               'Datos Personales',
@@ -94,28 +111,75 @@ class _LoginPageState extends State<RegisterUserData> {
                   cont += 1;
                 },
               ),
-              MyCustomButtoms(
-                hintText: "Entrar",
-                colors: [
-                  Color(0xFF19AEFF),
-                  Color(0xFF139DF7),
-                  Color(0xFF0A83EE),
-                  Color(0xFF0570E5),
-                  Color(0xFF0064E0)
-                ],
-                onPressed: (){sendCommet(context);},
-              ),
+              SizedBox(height: 20),
+
             ]));
   }
+  Widget _numberForm(BuildContext context) {
+    final userInfo = Provider.of<UserInfo>(context);
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                  child: new Flexible(
+                    child: CountryCodePicker(
+                    countryFilter: ['CO'],
+                    onChanged: print,
+                    initialSelection: 'CO',
+                    favorite: ['+57', 'CO'],
+                    showCountryOnly: true,
+                    showOnlyCountryWhenClosed: false,
+                    alignLeft: false,
+                    ),
+                  )
+                ),
+                new Container(
+                  child: new Flexible(
+                    child: TextFormField(
+                      inputFormatters: <TextInputFormatter>[
+                        WhitelistingTextInputFormatter.digitsOnly
+                        ],
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        hintText: 'Celular',
+                      ),
+                      validator: (String value) {
+                        if (value.length < 10) {
+                          return 'Digita un número de teléfono valido';
+                        }
+                        return null;
+                      },
+                      onSaved: (String value) {
+                        userInfo.phone= int.parse(value);
+                      },
+                    )
+                  ),
+                ),
+               
+              ]
 
-  _subimit(context) {
+            ),
+          ]
+        )
+      )
+     
+    );
+  }
+
+  _subimit(context,String name, String email ,String method, String  publicity) {
     final userInfo = Provider.of<UserInfo>(context);
     final prefs = new PreferenciasUsuario();
 
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      var res = sendDataProvider.singUp(
-          userInfo.phone, model.name, model.email,"PHONE", userInfo.publi);
+      var res = sendDataProvider.singUp(prefs.token,name,email,method,publicity);
       sendToken.sendToken(prefs.token, prefs.tokenPhone.toString());
 
       res.then((response) async {
@@ -136,7 +200,7 @@ class _LoginPageState extends State<RegisterUserData> {
     
   }
 
-  sendCommet(context) {
+  sendCommet(context,String name, String email ,String method, String  publicity) {
     final userInfo = Provider.of<UserInfo>(context);
     if(_formKey.currentState.validate()){
     Alert(
@@ -149,7 +213,7 @@ class _LoginPageState extends State<RegisterUserData> {
               if (userInfo.publi == 'null') {
                 showToast("Por favor seleciona un  elemento*", Colors.red);
               } else {
-                _subimit(context);
+                _subimit(context,name,email,method,publicity);
               }
             },
             child: Text(
