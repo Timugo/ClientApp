c92b4586bd58e93167954e030504dff6cbf902eb
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-24 08:34:17 -0500

    chore() : improve documentation

--- lib/src/providers/barber_provider.dart
+++ lib/src/providers/barber_provider.dart
@@ -1,21 +1,20 @@
+// Flutter dependencies
 import 'package:flutter/material.dart';
 
 class BarberAsigned with ChangeNotifier {
-
+  // Properties
   String _phone = '000-000-000';
   String _name='Sin Asignar';
   String _urlImg = '';
   double _stairs = 0.0;
   
- 
-
+  //Getters
   get phone {
     return _phone;
   }
   get name {
     return _name;
   }
-
   get urlImg {
     return _urlImg;
   }
@@ -23,24 +22,21 @@ class BarberAsigned with ChangeNotifier {
     return _stairs;
   }
   
-
-  set phone( String nombre ) {
-    this._phone = nombre;
+  // Setters
+  set phone( String value ) {
+    this._phone = value;
     notifyListeners();
   }
-  set name( String nombre ) {
-    this._name = nombre;
+  set name( String value ) {
+    this._name = value;
     notifyListeners();
   }
-
-  set urlImg( String nombre ) {
-    this._urlImg = nombre;
+  set urlImg( String value ) {
+    this._urlImg = value;
     notifyListeners();
   }
-  set stairs( double nombre ) {
-    this._stairs = nombre;
+  set stairs( double value ) {
+    this._stairs = value;
     notifyListeners();
   }
-
-
 }
