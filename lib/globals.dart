a4a625050a7a6b84ec30b6b38978faef3bd1f671
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-25 17:15:59 -0500

    chore(): testing endpoints

--- lib/globals.dart
+++ lib/globals.dart
@@ -5,11 +5,11 @@
 // FLutter dependencies
 library my_prj.globals;
 /* Production URL */
-String url = 'https://api.timugo.com/';
-String urlV2 = 'https://apiv2.timugo.com/';
+//String url = 'https://api.timugo.com/';
+//String urlV2 = 'https://apiv2.timugo.com/';
 /* Sanbox URL */
-//String url ='https://sandbox-nodejs.herokuapp.com/'; 
-//String urlV2 ='https://sandbox-nest.herokuapp.com/'; 
+String url ='https://sandbox-nodejs.herokuapp.com/'; 
+String urlV2 ='https://sandbox-nest.herokuapp.com/'; 
 
 /*data url*/
 String dataUrl = 'https://data.timugo.com/';
