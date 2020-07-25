af1af70f38d0edcdef220675543894e3b122de99
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-25 17:16:35 -0500

    chore(): improve code reusability

--- lib/src/pages/homeservices/application/widgets/cardservices_widget.dart
+++ lib/src/pages/homeservices/application/widgets/cardservices_widget.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 /* Enviroments*/
 import 'package:timugo/globals.dart' as globals;
+import 'package:timugo/src/interfaces/server_response.dart';
 /* Pages */
 import 'package:timugo/src/pages/checkin/application/checkin_page.dart';
 import 'package:timugo/src/pages/homeservices/domain/services_model.dart';
@@ -17,52 +20,53 @@ class CardsServices extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final servicesProvider = ServicesProvider();
     return FutureBuilder(
-        future: servicesProvider.getServices(),
-        builder: (BuildContext context,
-            AsyncSnapshot<List<ServicesModel>> snapshot) {
-          if (snapshot.hasData) {
-            final productos = snapshot.data;
-            return Container(
-              width: size.width,
-              height: size.width > size.height
-                  ? size.height * 0.60
-                  : size.height * 0.40,
-              child: PageView.builder(
-                controller: PageController(
-                    viewportFraction: size.width > size.height ? 0.4 : 0.7),
-                pageSnapping: false,
-                itemCount: productos.length,
-                itemBuilder: (context, i) => _Card(productos[i]),
+      future: servicesProvider.getServices(),
+      builder: (context,AsyncSnapshot<List<ServicesModel>> snapshot) {
+        if (snapshot.hasData) {
+          final services = snapshot.data;
+          return Container(
+            width: size.width,
+            height: size.width > size.height
+                ? size.height * 0.60
+                : size.height * 0.40,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: size.width > size.height ? 0.4 : 0.7
               ),
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        });
+              pageSnapping: false,
+              itemCount: services.length,
+              itemBuilder: (context, i) => _Card(services[i]),
+            ),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      });
   }
 }
 
 class _Card extends StatelessWidget {
   final  ServicesModel prod;
+  //Constructor
   _Card(this.prod);
   
   @override
   Widget build(BuildContext context) {
     // Data server Url
-    final String  dataUrl = globals.url;
+    final String  url = globals.url;
     final size = MediaQuery.of(context).size;
     final userInfo = Provider.of<UserInfo>(context);
     userInfo.urlImg = prod.urlImg;
     return GestureDetector(
       onTap: (){
-        _onTap(context);
+        _onCardTap(context);
       },
       child:Container(
         child: Stack(
           children: <Widget>[
             Row(
               children: <Widget>[
-                _DescriptionCard(prod),
+                _DescriptionCard(prod,_onCardTap(context)),
               ],
             ),
           
@@ -70,7 +74,7 @@ class _Card extends StatelessWidget {
               top: size.width>size.height ? size.height*0.0 : size.height*0.0,
               bottom: size.width>size.height ? size.height*0.14 : size.height*0.11,
               left: size.width>size.height ? size.width*0.05 : size.width*0.09,
-              child: Image.network(dataUrl+prod.urlImg,
+              child: Image.network(url+prod.urlImg,
                 width: size.width>size.height ? size.height*0.22 : size.height*0.15,
                 height: size.width>size.height ? size.height*0.22 : size.height*0.15,
               )
@@ -81,101 +85,92 @@ class _Card extends StatelessWidget {
     );
   }
 
-  _onTap(context) {
+  _onCardTap(context) {
     final checkUserOrder = CheckUserOrder();
-    var res = checkUserOrder.checkUserOrder();
-    res.then((response) async {
-      if (response['response'] == 1) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => Checkin(model: prod)));
-      } else {
-        showToast("Aún no tienes ordenes en curso!", Color(0xFF0570E5),);
-      }
-    });
+    checkUserOrder.checkUserOrder()
+      .then((response) {
+        print(response.body);
+        //final resp = iServerResponseFromJson(response.body);
+        var res = jsonDecode(response.body); 
+        if (res["response"] == 1) {
+          Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Checkin(model: prod))
+          );
+        } else if(res["response"] ==2 ) {
+          showToast("Aún tienes una orden en curso!", Color(0xFF0570E5));
+        }
+      })
+      .catchError((onError){
+        print(onError);
+      });
   }
-  
 
 }
 class _DescriptionCard extends StatelessWidget {
   final ServicesModel prod;
-
-  _DescriptionCard(this.prod);
+  final Function _onTap;
+  _DescriptionCard(this.prod,this._onTap);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return new GestureDetector(
-      onTap: (){
-         _onTap(context);
-      },   
+      onTap: () => _onTap(),   
       child: Card(  
-      elevation: 8,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: Container(
-          width:size.width>size.height ? size.height*0.40 :size.height*0.28, ///////////////////////////////////////
-          height: size.height,
-          color : Color(0xff000000),
-          child: Column(
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Container(
+            width:size.width>size.height ? size.height*0.40 :size.height*0.28, ///////////////////////////////////////
+            height: size.height,
+            color : Color(0xff000000),
+            child: Column(
 
-            children: <Widget>[
-              Spacer(),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: <Widget>[
-                    Spacer(),
-                    Text('${prod.name}',style: TextStyle(color: Colors.white,fontSize: 14,fontWeight: FontWeight.bold)),
-                    Spacer(),
-                  ],
+              children: <Widget>[
+                Spacer(),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Spacer(),
+                      Text('${prod.name}',style: TextStyle(color: Colors.white,fontSize: 14,fontWeight: FontWeight.bold)),
+                      Spacer(),
+                    ],
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    child: Text('\$'+'${prod.price}',style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold),),
-                    width: size.width>size.height ? size.width*0.1 : size.width*0.17,
-                    //height: size.height*0.05,
-                  ),
-                  Container(
-                    child:RaisedButton(
-                      color: Color(0xFF0570E5),
-                      child:Text('Solicitar',style: TextStyle(color: Colors.white)),
-                      onPressed: (){
-                        _onTap(context);
-                      }
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      child: Text('\$'+'${prod.price}',style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold),),
+                      width: size.width>size.height ? size.width*0.1 : size.width*0.17,
+                      //height: size.height*0.05,
                     ),
-                    width: 100.0,
-                    height: 35,
-                    decoration:BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15))
-                    ) ,
-                  ),
-                ],
-              )
-            ],
+                    Container(
+                      child:RaisedButton(
+                        color: Color(0xFF0570E5),
+                        child:Text('Solicitar',style: TextStyle(color: Colors.white)),
+                        onPressed: () => _onTap
+                      ),
+                      width: 100.0,
+                      height: 35,
+                      decoration:BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(15))
+                      ) ,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    )
+      )
     );  
   }
-  
-  _onTap(context){
-    final checkUserOrder =CheckUserOrder();
-    var res = checkUserOrder.checkUserOrder();
-    res.then((response) async {
-      if (response['response'] == 1){
-        Navigator.push(
-        context,MaterialPageRoute(
-        builder: (context) => Checkin(model:prod)));
 
-      }else{
-        
-      }
-    });                  
-  }
 }
+
+
+
